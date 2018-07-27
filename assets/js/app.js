9117c5335e06459980996e44a23e986cdf81a9b2
mskian <veerfun23@gmail.com>
2018-07-27 14:31:53 +0530

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,14 +4,15 @@ fetch('https://quotes.santhoshveer.com/quoteswritten.json')
 
 console.log(data)
 
-$(document).ready(function() { 
-$('#livescore').on('click', function() { 
 var quotesContent = data;
 var random = quotesContent.quoteswritten[Math.floor(Math.random() * quotesContent.quoteswritten.length)];
 const quotesdata = document.getElementById('sanquotes');
 //document.body.innerHTML = random.quotes;
 document.getElementById("quotesdata").innerHTML = random.quotes;
-$('#div1-wrapper').load('#quotesdata'); 
 });
-});
-});
+
+$(document).ready(function() {
+    $('#livescore').click(function() {
+        location.reload();
+    });
+}); 
