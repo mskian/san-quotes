bac8c58527dff6f935cdefa6dafdf7ebe84d1029
mskian <veerfun23@gmail.com>
2018-07-27 19:56:54 +0530

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,42 @@
-'use strict';
+"use strict";
 
-fetch('https://quotes.santhoshveer.com/quoteswritten.json')
+/**
+ * San Quotes - https://quotes.santhoshveer.com
+ * Developed by Santhosh veer (https://santhoshveer.com)
+ * File - app.js
+ * Version - 1.0.1
+ * Updated at - 27-07-2018
+ */
+
+//REST API
+const endpoint = "https://quotes.santhoshveer.com/quoteswritten.json";
+
+// FETCH API
+fetch(endpoint)
 .then(response => response.json())
 .then(data => {
 
+// GEt JSON Output on Browser Console log
 console.log(data)
 
+// Generate Random Quotes
+function genQuote() {
 var quotesContent = data;
 var random = quotesContent.quoteswritten[Math.floor(Math.random() * quotesContent.quoteswritten.length)];
-const quotesdata = document.getElementById('sanquotes');
-//document.body.innerHTML = random.quotes;
+//const quotesdata = document.getElementById("sanquotes");
 document.getElementById("quotesdata").innerHTML = random.quotes;
-});
+}
 
+// wait for page load before displaying content
 $(document).ready(function() {
-    $('#livescore').click(function() {
-        location.reload();
-    });
-}); 
+
+  // load an initial quote
+   genQuote();
+
+  // retrieve a new quote when the button is clicked
+  $("#generateQuotes").on("click", function() {
+    genQuote();
+
+   });
+  });
+});
