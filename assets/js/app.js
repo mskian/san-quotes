4bd1ddfec3d1187dbbffa8d8e13fe7a2ed3c748a
mskian <veerfun23@gmail.com>
2018-07-27 13:34:30 +0530

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,12 +4,13 @@ fetch('https://quotes.santhoshveer.com/quoteswritten.json')
 
 console.log(data)
 
-$("#livescore").on("click", function(t) {
-t.preventDefault()
+
 var quotesContent = data;
 var random = quotesContent.quoteswritten[Math.floor(Math.random() * quotesContent.quoteswritten.length)];
 const quotesdata = document.getElementById('sanquotes');
 //document.body.innerHTML = random.quotes;
+$("#livescore").on("click", function(t) {
+    t.preventDefault()
 document.getElementById("quotesdata").innerHTML = random.quotes;
    });
 }); 
