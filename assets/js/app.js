b3b1a2c3e14d9686d2ae353cbb06598fa1bcb67f
mskian <veerfun23@gmail.com>
2018-07-27 14:30:38 +0530

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,14 +4,14 @@ fetch('https://quotes.santhoshveer.com/quoteswritten.json')
 
 console.log(data)
 
+$(document).ready(function() { 
+$('#livescore').on('click', function() { 
 var quotesContent = data;
 var random = quotesContent.quoteswritten[Math.floor(Math.random() * quotesContent.quoteswritten.length)];
 const quotesdata = document.getElementById('sanquotes');
 //document.body.innerHTML = random.quotes;
 document.getElementById("quotesdata").innerHTML = random.quotes;
+$('#div1-wrapper').load('#quotesdata'); 
 });
-
-$("#livescore").click(function() {
-    //alert('clicked')
-    $("#quotesdata").load(" #quotesdata > *");
-  }); 
+});
+});
